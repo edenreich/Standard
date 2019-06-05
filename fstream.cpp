0a91182a7bd56417029662f48d1282f75fb80fb5
Eden Reich <eden.reich@gmail.com>
2019-06-06 00:24:15 +0200

    simply get line by line example from a file

--- fstream.cpp
+++ fstream.cpp
@@ -3,7 +3,22 @@
 
 int main()
 {
+    std::string filename = "fixtures/template.h";
 
+    std::ifstream input;
+
+    input.open(filename);
+
+    while (!input.eof())
+    {
+        std::string description;
+
+        std::getline(input, description, '\n');
+
+        std::cout << "line: " << description << std::endl;
+    }
+
+    input.close();
 
     return 0;
 }
